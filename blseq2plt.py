cd18443d7963d5d79e721cbabad04d6491585d66
Andrew Elwell <Andrew.Elwell@gmail.com>
2011-04-18 20:18:54 +0200

    Generates png for each BLSEQ in input file
    * Generates a png file for each data stream - use with a small
      logfile...

--- blseq2plt.py
+++ blseq2plt.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python
 
-# blseq2plt.py - Generate gnuplot output from logfile
+# blseq2plt.py - Generate plot output from logfile
 # Andrew Elwell <Andrew.Elwell@gmail.com>
 # Licenced under GPL2+
 
@@ -9,12 +9,15 @@
 # into the data format.
 
 import struct
+import matplotlib
+matplotlib.use('Agg')
 import matplotlib.pyplot as plt
 
 logfile = open("sample.dat")
 try:
 	for line in logfile:
 		line = line.rstrip()
+		# we should really check that ident == '$BLSEQ'
 		(ident, timestamp, data) = line.split(',')
 		(data,checksum) = data.split('*')
 		# split data string into chunks for each pair of readings
@@ -22,18 +25,19 @@ try:
 		chan1 = []
 		chan2 = []
 		readings = [data[k:k+4] for k in xrange(0,len(data),4)]
-		plot = []
 		for v in readings:
 			c1,c2 = struct.unpack('B B',v.decode('hex'))
 			chan1.append(c1)
 			chan2.append(c2)
+		fig = plt.figure()
+		plt.plot(chan1, label='Channel 1')
+		plt.plot(chan2, label='Channel 2')
+		plt.title(timestamp)
+		plt.legend()
+		#plt.show()
+		fig.savefig(timestamp + '.png')
+
 			
 finally:
 	logfile.close()
-
-plt.plot(chan1, label='Channel 1')
-plt.plot(chan2, label='Channel 2')
-plt.title(timestamp)
-plt.legend()
-plt.show()
 
